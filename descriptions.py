049272e7a7b67e33dab3dff8ccfeb4efef91a98d
Wersario <promodewerc@mail.ru>
2022-05-04 16:54:43 +0300

    Added descriptions to all commands

---
+++ descriptions.py
@@ -0,0 +1,14 @@
+desc = {
+    'make_diagram': '''
+    Creates a circle diagram of ratio between unknown jobs, casual jobs, and programmer jobs.
+    Needs 2 arguments: start - id of the first vacation and end - id of the last vacation. It will create an interval,
+    where function will compare amounts.
+    ''',
+    'get_vacancies': '''
+    Gives you all programmer vacancies in chosen interval. Needs 2 arguments: start - id of the first vacation and end
+    - id of the last vacation.
+    '''
+}
+
+key_words = ['Программист', 'Programmer', 'Developer', 'Программист-разработчик', 'Разработчик', 'разработчик',
+             'программист', 'developer']
