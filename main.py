7ce4f428a36edd8927e7a48a96f6528b992215af
Wersario <60400132+Wersario@users.noreply.github.com>
2022-05-11 19:58:16 +0300

    Deleted all discord parts. Added .lower() to the checking

--- main.py
+++ main.py
@@ -1,82 +1,25 @@
-import discord
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from discord.ext import commands
 from descriptions import desc, key_words
 
 
-# getting your token from the token.txt
-def read_token():
-    with open('token.txt', 'r') as f:
-        lines = f.readlines()
-        return lines[0].strip()
+LINK = 'https://api.hh.ru/vacancies/'
+VACANCY = 'https://hh.ru/vacancy/'
 
 
-# logging in your bot
-TOKEN = read_token()
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all(), help_command=None)
-bot.remove_command('help')
-bot.run('TOKEN')
-
-
-# creating custom help command
-@bot.command()
-async def help(ctx, args=None):
-    '''
-        Creates a circle diagram of ratio between unknown jobs, casual jobs, and programmer jobs.
-        Needs 2 arguments: start - id of the first vacation and end - id of the last vacation. It will create an interval,
-        where function will compare amounts.
-    '''
-    embed = discord.Embed(title="I am here to help you.")
-    command_names_list = [com.name for com in bot.commands]
-
-    if not args:
-        embed.add_field(
-            name="List of supported commands:",
-            value="\n".join([str(i+1)+". "+com.name for i, com in enumerate(bot.commands)]),
-            inline=False
-        )
-        embed.add_field(
-            name="Details",
-            value="Type `>help [command name]` for more details about each command.",
-            inline=False
-        )
-    elif args in command_names_list:
-        embed.add_field(
-            name=args,
-            value=desc[str(args)]
-        )
-    else:
-        embed.add_field(
-            name="Uhh....",
-            value="There is no command like this."
-        )
-
-    await ctx.send(embed=embed)
-
-
-# Starting a bot
-@bot.event
-async def on_ready():
-    print('Bot started.')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Wersario`s development'))
-
-
-@bot.command()
-async def make_diagram(ctx, start, end):
-    link = 'https://api.hh.ru/vacancies/'
+def make_diagram(start, end):
     stats = np.array([0, 0, 0])
     labels = ['Unknown', 'Other Jobs', 'Programmers']
     check = 0
     for num in range(start, end):
-        r = requests.get(link + str(num))
-        if r.json()['description'] == 'Not Found':
+        response = requests.get(LINK + str(num)).json()
+        if response['description'] == 'Not Found':
             stats[0] += 1
             continue
         for word in key_words:
-            if word in r.json()['name']:
+            if word in response['name'].lower():
                 check = 1
                 break
         if check == 1:
@@ -86,38 +29,29 @@ async def make_diagram(ctx, start, end):
             stats[1] += 1
 
     plt.pie(stats, labels=labels)
-    embed = discord.Embed(title="Here is your diagram.")
-    embed.set_image(plt.show())
-    await ctx.channel.send(embed=embed)
+    plt.show()
 
 
-@bot.command()
-async def get_vacancies(ctx, start, end):
+def get_vacancies(start, end):
     '''
         Gives you all programmer vacancies in chosen interval. Needs 2 arguments: start - id of the first vacation and end
         - id of the last vacation.
     '''
-    link = 'https://api.hh.ru/vacancies/'
-    template = 'https://hh.ru/vacancy/'
     for num in range(start, end):
-        r = requests.get(link + str(num))
-        if r.json()['description'] == 'Not Found':
+        response = requests.get(LINK + str(num)).json()
+        if response['description'] == 'Not Found':
             continue
         for word in key_words:
-            if word in r.json()['name']:
-                await ctx.channel.send(f'{r.json()["name"]}, опыт работы: {r.json()["experience"]["name"]}. Ссылка: {template+str(num)}')
+            if word in response['name'].lower():
+                print(f'{response["name"]}, опыт работы: {response["experience"]["name"]}. Ссылка: {TEMPLATE + str(num)}')
+     print('Больше вакансий нет')
 
-        await ctx.channel.send('That`s all!')
 
-
-@bot.command()
-async def make_dataset(ctx, start, end):
+def make_dataset(start, end):
     '''
         Creates a data set about all vacancies.  Needs 2 arguments: start - id of the first vacation and end -
         id of the last vacation.
     '''
-    link = 'https://api.hh.ru/vacancies/'
-    template = 'https://hh.ru/vacancy/'
     data = {
         'Name': [],
         'Experience': [],
@@ -127,28 +61,28 @@ async def make_dataset(ctx, start, end):
         'Link': []
     }
     for num in range(start, end):
-        r = requests.get(link + str(num))
+        response = requests.get(LINK + str(num)).json()
         if r.json()['description'] == 'Not Found':
             continue
         for word in key_words:
-            if word in r.json()['name']:
-                data['Name'].append(r.json()['Name'])
-                if r.json()['experience'] is None:
+            if word in response['name'].lower():
+                data['Name'].append(response['Name'])
+                if response['experience'] is None:
                     data['Experience'].append(np.nan)
                 else:
-                    data['Experience'].append(r.json()['experience'])
+                    data['Experience'].append(response['experience'])
                 if r.json()['salary']['from'] is None:
                     data['Salary'].append(np.nan)
                 else:
-                    data['Salary'].append(f"{r.json()['salary']['from'] + r.json()['salary']['currency']}")
+                    data['Salary'].append(f"{response['salary']['from'] + r.json()['salary']['currency']}")
                 if r.json()['address']['city'] is None:
                     data['City'].append(np.nan)
                 else:
-                    data['City'].append(r.json()['address']['city'])
+                    data['City'].append(response['address']['city'])
                 if r.json()['address']['street'] is None:
                     data['Street'].append(np.nan)
                 else:
-                    data['Street'].append(r.json()['address']['street'])
-                data['Link'].append(template + str(num))
+                    data['Street'].append(response['address']['street'])
+                data['Link'].append(TEMPLATE + str(num))
 
-     await ctx.channel.send(pd.DataFrame(data))
+     print(pd.DataFrame(data))
