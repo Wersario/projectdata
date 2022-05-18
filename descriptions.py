82d10858be77e2a103f39055e991246e847ecd9f
Wersario <promodewerc@mail.ru>
2022-05-18 17:48:46 +0300

    Final commits

--- descriptions.py
+++ descriptions.py
@@ -1,17 +1,1 @@
-desc = {
-    'make_diagram': '''
-    Creates a circle diagram of ratio between unknown jobs, casual jobs, and programmer jobs.
-    Needs 2 arguments: start - id of the first vacation and end - id of the last vacation. It will create an interval,
-    where function will compare amounts.
-    ''',
-    'get_vacancies': '''
-    Gives you all programmer vacancies in chosen interval. Needs 2 arguments: start - id of the first vacation and end
-    - id of the last vacation.
-    ''',
-    'make_dataset': '''
-    Creates a data set about all vacancies.  Needs 2 arguments: start - id of the first vacation and end -
-    id of the last vacation.
-    '''
-}
-
-key_words = [ 'разработчик', 'программист', 'developer']
+key_words = ['разработчик', 'программист', 'developer']
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from descriptions import desc, key_words
+from descriptions import key_words
 
 
 LINK = 'https://api.hh.ru/vacancies/'
@@ -10,6 +10,11 @@ VACANCY = 'https://hh.ru/vacancy/'
 
 
 def make_diagram(start, end):
+    """
+    Creates a circle diagram of ratio between unknown jobs, casual jobs, and programmer jobs.
+    Needs 2 arguments: start - id of the first vacation and end - id of the last vacation. It will create an interval,
+    where function will compare amounts.
+    """
     stats = np.array([0, 0, 0])
     labels = ['Unknown', 'Other Jobs', 'Programmers']
     check = 0
@@ -27,31 +32,32 @@ def make_diagram(start, end):
             check = 0
         else:
             stats[1] += 1
-
+    fig, ax = plt.subplots()
+    ax.pie(stats, labels=labels)
+    ax.axis("equal")
     plt.pie(stats, labels=labels)
-    plt.show()
 
 
 def get_vacancies(start, end):
-    '''
+    """
         Gives you all programmer vacancies in chosen interval. Needs 2 arguments: start - id of the first vacation and end
         - id of the last vacation.
-    '''
+    """
     for num in range(start, end):
         response = requests.get(LINK + str(num)).json()
         if response['description'] == 'Not Found':
             continue
         for word in key_words:
             if word in response['name'].lower():
-                print(f'{response["name"]}, опыт работы: {response["experience"]["name"]}. Ссылка: {TEMPLATE + str(num)}')
-     print('Больше вакансий нет')
+                print(f'{response["name"]}, опыт работы: {response["experience"]["name"]}. Ссылка: {VACANCY + str(num)}')
+    print('Больше вакансий нет')
 
 
-def make_dataset(start, end):
-    '''
+def make_data_set(start, end):
+    """
         Creates a data set about all vacancies.  Needs 2 arguments: start - id of the first vacation and end -
         id of the last vacation.
-    '''
+    """
     data = {
         'Name': [],
         'Experience': [],
@@ -62,27 +68,39 @@ def make_dataset(start, end):
     }
     for num in range(start, end):
         response = requests.get(LINK + str(num)).json()
-        if r.json()['description'] == 'Not Found':
+        if response["description"] == 'Not Found':
             continue
         for word in key_words:
             if word in response['name'].lower():
-                data['Name'].append(response['Name'])
+                data['Name'].append(response['name'])
                 if response['experience'] is None:
                     data['Experience'].append(np.nan)
                 else:
-                    data['Experience'].append(response['experience'])
-                if r.json()['salary']['from'] is None:
+                    data['Experience'].append(response['experience']['name'])
+                if response['salary'] is None:
                     data['Salary'].append(np.nan)
                 else:
-                    data['Salary'].append(f"{response['salary']['from'] + r.json()['salary']['currency']}")
-                if r.json()['address']['city'] is None:
+                  if response['salary']['from'] is None:
+                    data['Salary'].append(np.nan)
+                  else:
+                    data['Salary'].append(f"{response['salary']['from'] + response['salary']['currency']}")
+                if response['address'] is None:
                     data['City'].append(np.nan)
-                else:
-                    data['City'].append(response['address']['city'])
-                if r.json()['address']['street'] is None:
                     data['Street'].append(np.nan)
                 else:
-                    data['Street'].append(response['address']['street'])
-                data['Link'].append(TEMPLATE + str(num))
+                  if response['address']['city'] is None:
+                    data['City'].append(np.nan)
+                  else:
+                    data['City'].append(response['address']['city'])
+                  if response['address']['street'] is None:
+                      data['Street'].append(np.nan)
+                  else:
+                      data['Street'].append(response['address']['street'])
+                data['Link'].append(VACANCY + str(num))
 
-     print(pd.DataFrame(data))
+    return pd.DataFrame(data)
+
+    print(pd.DataFrame(data))
+
+
+get_vacancies(1, 100000)
